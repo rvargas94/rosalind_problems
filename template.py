28224a2e345358dad29993fd04d77a145cc8c121
Richard Vargas <Rvargas94@LIVE.COM>
2021-04-27 21:56:01 -0700

    added template for future problem solutions

---
+++ template.py
@@ -0,0 +1,12 @@
+######################################
+#This script will solve the problem  #
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXX       #
+#From http://rosalind.info/problems  #
+######################################
+
+def main():
+    file_to_open = ""
+    open_file_buffer = open(file_to_open,"r")
+
+if __name__ == '__main__':
+    main()
